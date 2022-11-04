02a17c8ee769d99e12171bf4dc5cc72b3f52dc38
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-11-03 21:38:06 -0700

    Create Teacher Object

---
+++ src/Teacher.java
@@ -0,0 +1,47 @@
+/**
+ * Object that represents a teacher and the subject that they teach
+ */
+public class Teacher {
+    private String firstName;
+    private String lastName;
+    private String subject;
+
+    public Teacher(String firstName, String lastName, String subject) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.subject = subject;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    /**
+     * Print out the teacher's full name and their subject
+     * @return the teacher's full name and their subject
+     */
+    @Override
+    public String toString() {
+        return String.format("Name: %s %s Subject %s", firstName, lastName, subject);
+    }
+}
